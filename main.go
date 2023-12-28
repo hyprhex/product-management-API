a00a3697ca67334af2b157aefd9505e625cedc0f
HyprHex <swapapis@gmail.com>
2023-12-28 15:29:17 +0100

    Create methods to connect and run DB, And gitigonre and env.example files

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
-package main 
+package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-    
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_DBNAME"),
+	)
+
+	a.Run(":8010")
 }
